702c01e5516d18b15a878113c4586077975d5b10
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-01-13 21:24:57 +0530

    fix:add timer util

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 
 #include "../utils/cat.h"
+#include "../utils/timer.h"
 #include "../utils/size.h"
 #include <cstdlib>
 #include <iostream>
@@ -14,6 +15,8 @@ void show_help() {
     std::cout << "  size <path>               Shows the space in the current path\n";
     std::cout << "  size                      Shows the space in home directory \n";
     std::cout << "  cAt!                      Generates random ASCII cat image\n";
+    std::cout << "  timer <minutes>           Sets a countdown timer for specified minutes\n";
+    std::cout << "  timer <minutes> <seconds> Sets a countdown timer for minutes and seconds\n";
     std::cout << "  --help                    Show this help message\n";
 }
 
@@ -21,6 +24,7 @@ enum Command {
     HELP,
     CAT,
     SIZE,
+    TIMER,
     UNKNOWN
 };
 
@@ -28,6 +32,7 @@ Command getCommand(std::string_view cmd) {
     if (cmd == "--help") return HELP;
     if (cmd == "cAt!") return CAT;
     if (cmd == "size") return SIZE;
+    if (cmd == "timer") return TIMER;
     return UNKNOWN;
 }
 
@@ -56,6 +61,21 @@ int main(int argc, char* argv[]) {
             } else if (argc == 3) {
                 std::cout << "Size of " << argv[2] << ": " 
                          << sizeCalc.getSize(fs::path(argv[2])) << std::endl;
+            }
+            break;
+        }
+
+        case TIMER: {
+            if (argc == 3) {
+                Timer timer(std::stoi(argv[2]));
+                timer.setTimer();
+            } else if (argc == 4) {
+                Timer timer(std::stoi(argv[2]), std::stoi(argv[3]));
+                timer.setTimer();
+            } else {
+                std::cerr << "Error: Timer requires minutes or minutes and seconds\n";
+                show_help();
+                return 1;
             }
             break;
         }
